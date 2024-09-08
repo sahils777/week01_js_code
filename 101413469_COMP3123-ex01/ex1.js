85ab3ca543fb52913f301d403438640ce6e9d834
Mohmadsahil Shaikh <101413469@georgebrown.ca>
2024-09-07 21:14:40 -0400

    link updated

--- 101413469_COMP3123-ex01/ex1.js
+++ 101413469_COMP3123-ex01/ex1.js
@@ -2,3 +2,5 @@ function capitalizeWords(str) {
     return str.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
   }
 console.log(capitalizeWords("the quick brown fox"));
+
+//https://github.com/sahils777/week01_js_code/tree/main/101413469_COMP3123-ex01
